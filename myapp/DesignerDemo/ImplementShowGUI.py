1a0bf6b5aa00d814050b3d59240b536af4ddd821
shireenrao <shireenrao@gmail.com>
2013-07-12 19:22:24 -0400

    Fix bug in Designer demo

--- myapp/DesignerDemo/ImplementShowGUI.py
+++ myapp/DesignerDemo/ImplementShowGUI.py
@@ -17,30 +17,10 @@ class MainDialog(QDialog, ShowGUI.Ui_mainDialog):
         self.showButton.setFocus()
         self.showButton.clicked.connect(self.processData)
         #self.connect(self.showButton, SIGNAL("clicked()"), self.showMessageBox)
-        self.workerThread = workerThread()
-        #self.workerThread.threadDone.connect(self.threadDone, Qt.DirectConnection)
-        #self.connect(self.workerThread, SIGNAL("threadDone(QString)"), self.threadDone, Qt.DirectConnection)
-        self.workerThread.threadone.connect(self.threadDone)
-        #self.threadone.connect(self.threadDone)
 
     def processData(self):
-        self.workerThread.start()
-        #QMessageBox.information(self, "Hello!", "Hello there, " + self.nameEdit.text())
+        QMessageBox.information(self, "Hello!", "Hello there, " + self.nameEdit.text())
 
-    def threadDone(self, txtMsg):
-        self.nameEdit.setText(txtMsg)
-
-class workerThread(QThread):
-
-    threadone = Signal(str)
-
-    def __init__(self, parent=None):
-        super(workerThread, self).__init__(parent)
-
-    def run(self):
-        time.sleep(6)
-        self.threadone.emit("Confirmation that the thread is complete.")
-        #self.emit(SIGNAL("threadDone(QString)"), "Confirmation that the thread is complete.")
 
 app = QApplication(sys.argv)
 form = MainDialog()
